dc450baa27fcfddbe43761855d7b6b9a84d038fd
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-03-09 23:12:36 -0800

    adding a unit test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
 
+#Edith goes to cool new todo list app homepage
 browser = webdriver.Firefox()
 
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+#She notices the page title and header mention to-do lists
+assert 'To-Do' in browser.title
+
+#She does other stuff
+
+browser.quit()
+
+#To run Django type in
+# python manage.py runserver
+# cd Documents/GitHub/TDD/superlists
