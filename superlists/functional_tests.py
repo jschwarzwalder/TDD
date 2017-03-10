bc4d96c22ba5c36e4f5daa86f1ab78a3aed31f60
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-03-09 23:22:39 -0800

    descriptive errors

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,8 @@
+import unittest
 from selenium import webdriver
+
+class NewVisitorTest(unittest.TestCase):
+	def test_starting_a_new_todo_list(self);
 
 #Edith goes to cool new todo list app homepage
 browser = webdriver.Firefox()
@@ -6,9 +10,31 @@ browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
 #She notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title
+assert 'To-Do' in browser.title, "Browser title was " + browser.title
 
 #She does other stuff
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box (Edith's hobby
+# is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effect.
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep
+
 
 browser.quit()
 
