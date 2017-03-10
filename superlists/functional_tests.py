1a1001a26ff509a8f3fb7cb2fc6e08806da86bb3
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-03-09 23:27:55 -0800

    using Unit Test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -2,41 +2,50 @@ import unittest
 from selenium import webdriver
 
 class NewVisitorTest(unittest.TestCase):
-	def test_starting_a_new_todo_list(self);
+	
+	def setUp(self):  
+        self.browser = webdriver.Firefox()
 
-#Edith goes to cool new todo list app homepage
-browser = webdriver.Firefox()
+    def tearDown(self):  
+        self.browser.quit()
 
-browser.get('http://localhost:8000')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+		#Edith goes to cool new todo list app homepage
+		browser = webdriver.Firefox()
 
-#She notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title, "Browser title was " + browser.title
+		browser.get('http://localhost:8000')
 
-#She does other stuff
+		#She notices the page title and header mention to-do lists
+		assert 'To-Do' in browser.title, "Browser title was " + browser.title
 
-# She is invited to enter a to-do item straight away
+		#She does other stuff
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+		# She is invited to enter a to-do item straight away
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+		# She types "Buy peacock feathers" into a text box (Edith's hobby
+		# is tying fly-fishing lures)
 
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		# When she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do list
 
-# The page updates again, and now shows both items on her list
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+		# The page updates again, and now shows both items on her list
 
-# She visits that URL - her to-do list is still there.
+		# Edith wonders whether the site will remember her list. Then she sees
+		# that the site has generated a unique URL for her -- there is some
+		# explanatory text to that effect.
 
-# Satisfied, she goes back to sleep
+		# She visits that URL - her to-do list is still there.
+
+		# Satisfied, she goes back to sleep
 
 
-browser.quit()
+		browser.quit()
+		
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore')
 
 #To run Django type in
 # python manage.py runserver
