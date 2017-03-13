cf5a2fbe8868e93b54996958e1c1339113e103bc
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-03-12 23:00:24 -0700

    first failed test

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,20 @@
+
 from django.test import TestCase
+from django.http import HttpRequest  
 
-# Create your tests here.
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-	
-	def test_maths(self):
-		self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')  
+		self.assertEqual(found.func, home_page)
+		
+	def test_home_page_is_about_todo_lists(self):
+		request = HttpRequest()
+		
+		response = home_page(request)
+		
+		self.assertTrue(response_content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do-Lists</title>', response_content)
+		self.assertTrue(response.content.endswith(b'</html>'))
