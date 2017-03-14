f16fb9dbfb30c49c9d64a40f16d1c56c71eff6ac
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-03-14 00:28:49 -0700

    built template for html

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,15 +5,18 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-	# def test_root_url_resolves_to_home_page_view(self):
-		# found = resolve('/')  
-		# self.assertEqual(found.func, home_page)
+    # def test_root_url_resolves_to_home_page_view(self):
+    # found = resolve('/')  
+    # self.assertEqual(found.func, home_page)
+        
+    def test_home_page_is_about_todo_lists(self):
+        request = HttpRequest()
+        
+        response = self.client.get('/') 
+            
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
 		
-	def test_home_page_is_about_todo_lists(self):
-		request = HttpRequest()
-		
-		response = home_page(request)
-		
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do-Lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+        
