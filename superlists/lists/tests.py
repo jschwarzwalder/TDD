989cb7d404cafd6ced8ae6a737d6078a088c2075
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-03-20 01:08:28 -0700

    Added CSRF Token to homepage

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,7 @@
+import re
 from django.test import TestCase
 from django.http import HttpRequest  
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -9,19 +11,23 @@ class HomePageTest(TestCase):
     # found = resolve('/')  
     # self.assertEqual(found.func, home_page)
         
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+    
+    def assertEqualExceptCSRF(self, html_code1, html_code2):
+        return self.assertEqual(
+            self.remove_csrf(html_code1),
+            self.remove_csrf(html_code2)
+        )
+    
     def test_home_page_is_about_todo_lists(self):
         request = HttpRequest()
         
-        response = self.client.get('/') 
-            
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do-Lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-        
-        with open('lists/templates/home.html') as f:
-            expected_content = f.read()
-        
-        self.assertEqual(html, expected_content)
+        #response = self.client.get('/') 
+        response = home_page(request)
+                    
+        expected_content = render_to_string('home.html', request=request)
+        self.assertEqualExceptCSRF(response.content.decode(), expected_content)
 		
-        
