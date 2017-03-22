5723f97cec786ca63f99c3ded432faf9d71513b2
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-03-22 00:19:28 -0700

    hard code solution to pass test. Very silly solution

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home_page(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
     return render(request, 'home.html')
